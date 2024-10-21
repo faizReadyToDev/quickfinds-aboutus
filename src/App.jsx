f29eaee1fa299f8cdda30373be74599998256286
Faiz Ur Rehman <faizurrehman1415@gmail.com>
2024-10-22 00:01:53 +0530

    Bug in tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AboutUs from '../components/AboutUs'
+import './App.css'
+import './index.css'
+
+
+function App() {
+
+  return (
+    <>
+     <AboutUs/>
+    </>
+  )
+}
+
+export default App
