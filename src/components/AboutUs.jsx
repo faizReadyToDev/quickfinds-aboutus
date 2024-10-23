a65fc00361fc3cdeaa9d47b2167cd41811dc684e
Faiz Ur Rehman <faizurrehman1415@gmail.com>
2024-10-23 12:14:39 +0530

    padding fixed

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -34,12 +34,12 @@ export default function AboutUs(){
                        {/* New heading for mobile : xs */}
                         <h1 className="lg:text-5xl sm:text-2xl xs:text-2xl font-bold font-poppins w-[40vw] xs:w-[100vw] xs:text-center hidden xs:block">Innovative solutions for a</h1>
                         <h1 className="lg:text-5xl sm:text-2xl xs:text-2xl font-bold font-poppins w-[40vw]  pb-[10px] xs:w-[100vw] xs:text-center hidden xs:block">better world!</h1>
-                    <p className="text-lg font-poppins w-[30vw] xl:text-base font-[400] lg:text-sm sm:text-xs xs:w-[70vw] xs:text-center xs:text-base">
+                    <p className="font-poppins md:w-[30vw] xl:text-base lg:text-sm sm:text-xs xs:w-[70vw] xs:text-center xs:text-[12px]">
                         We help businesses like yours earn more customers,standout from competitors,  make more money
                         </p>
                        
                         <span className="">
-                            <button className="bg-[#51B504] text-[#FFFFFF] lg:pt-[20px] lg:pr-[30px] lg:pb-[20px] lg:pl-[30px] lg:rounded-[16px] lg:text-lg lg:mt-[10px] font-bold sm:pt-[2px] sm:pb-[2px] sm:pr-[10px] sm:pl-[10px] sm:rounded-[5px] sm:text-[12px] xs:text-[12px] xs:pt-[2px] xs:pb-[2px] xs:pr-[10px] xs:pl-[10px] xs:rounded-[5px] xs:font-[400]">Get Started</button>
+                            <button className="bg-[#51B504] text-[#FFFFFF] lg:pt-[20px] lg:pr-[30px] lg:pb-[20px] lg:pl-[30px] lg:rounded-[16px] lg:text-lg lg:mt-[10px] xs:mt-[7px] font-bold sm:pt-[2px] sm:pb-[2px] sm:pr-[10px] sm:pl-[10px] sm:rounded-[5px] sm:text-[12px] xs:text-[12px] xs:pt-[2px] xs:pb-[2px] xs:pr-[10px] xs:pl-[10px] xs:rounded-[5px] xs:font-[400]">Get Started</button>
 
                         </span>
                     </div>
@@ -51,7 +51,7 @@ export default function AboutUs(){
                 </div>
 
                 {/* Animation */}
-                <div className="logo-animation bg-white lg:pt-[30px] lg:pb-[30px] overflow-hidden xs:mt-[15px] xs:pt-[0px] xs:pb-[0px] sm:pt-[15px] sm:pb-[15px]">
+                <div className="logo-animation bg-white lg:pt-[30px] lg:pb-[30px] overflow-hidden xs:mt-[10px] xs:pt-[0px] xs:pb-[0px] sm:pt-[15px] sm:pb-[15px]">
                     <div className="logo-slide flex items-center  gap-[30px] whitespace-nowrap lg:h-[10vh] md:h-[5vh] xs: h-[7vh]">
 
 
